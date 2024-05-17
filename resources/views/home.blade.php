6088302d4da879a41a432b4024bba5e3100bdb69
Dhimas_Mbuh <ephanzen4@gmail.com>
2024-05-17 11:08:58 +0700

    Slicing Home, Login, Signup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,6 +53,174 @@
             </div>
         </div>
     </nav>
+    <section class="container hero">
+        <div class="row align-items-center h-100">
+            <div class="col-12 col-lg-6">
+                <h1>
+                    Welcome to Laracuss <br> The Community Forum
+                </h1>
+                <p class="mb-4">
+                    Empowered by Laravel, Laracuss is a community forum for developers of all skill levels.
+                </p>
+                <a href="#" class="btn btn-primary me-2 mb-2 mb-lg-0">Sign Up</a>
+                <a href="#" class="btn btn-secondary mb-2 mb-lg-0">Join Discussion</a>
+            </div>
+            <div class="col-12 col-lg-6 h-315px order-first order-lg-last mb-2 mb-lg-0">
+                <img class="hero-image float-lg-end" src="{{ url('assets/images/hero-image.png') }}" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="container min-h-372px">
+        <div class="row">
+            <div class="col-12 col-lg-4 text-center">
+                <img class="promote-icon mb-2" src="{{ url('assets/images/discussions.png') }}" alt="Discussions">
+                <h2>Discussions</h2>
+                <p class="fs-3">12333</p>
+            </div>
+            <div class="col-12 col-lg-4 text-center">
+                <img class="promote-icon mb-2" src="{{ url('assets/images/answers.png') }}" alt="Answers">
+                <h2>Answers</h2>
+                <p class="fs-3">76612</p>
+            </div>
+            <div class="col-12 col-lg-4 text-center">
+                <img class="promote-icon mb-2" src="{{ url('assets/images/users.png') }}" alt="Users">
+                <h2>Users</h2>
+                <p class="fs-3">09822</p>
+            </div>
+        </div>
+    </section>
+    <section class="bg-gray">
+        <div class="container py-80px">
+            <h2 class="text-center mb-5">Help Others</h2>
+            <div class="row">
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card">
+                        <a href="#">
+                            <h3>How to add a custom validation in laravel?</h3>
+                        </a>
+                        <div class="">
+                            <p class="mb-5">I am working on a blogging application in Laravel 8. There are 4 user
+                                roles, among which, the ...</p>
+                        </div>
+                        <div class="row">
+                            <div class="col me-1 me-lg-2">
+                                <a href="#"><span class="badge rounded-pill text-bg-light">Eloquent</span></a>
+                            </div>
+                            <div class="col-5 col-lg-7">
+                                <div class="avatar-sm-wrapper d-inline-block">
+                                    <a href="#" class="me-1">
+                                        <img class="avatar rounded-circle"
+                                            src="{{ url('assets/images/avatar-sm.png') }}" alt="">
+                                    </a>
+                                </div>
+                                <span class="fs-12px">
+                                    <a href="#" class="me-1 fw-bold">dhimas</a>
+                                    <span class="color-gray">7 hours ago</span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card">
+                        <a href="#">
+                            <h3>How to add a custom validation in laravel?</h3>
+                        </a>
+                        <div class="">
+                            <p class="mb-5">I am working on a blogging application in Laravel 8. There are 4 user
+                                roles, among which, the ...</p>
+                        </div>
+                        <div class="row">
+                            <div class="col me-1 me-lg-2">
+                                <a href="#"><span class="badge rounded-pill text-bg-light">Eloquent</span></a>
+                            </div>
+                            <div class="col-5 col-lg-7">
+                                <div class="avatar-sm-wrapper d-inline-block">
+                                    <a href="#" class="me-1">
+                                        <img class="avatar rounded-circle"
+                                            src="{{ url('assets/images/avatar-sm.png') }}" alt="">
+                                    </a>
+                                </div>
+                                <span class="fs-12px">
+                                    <a href="#" class="me-1 fw-bold">dhimas</a>
+                                    <span class="color-gray">7 hours ago</span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card">
+                        <a href="#">
+                            <h3>How to add a custom validation in laravel?</h3>
+                        </a>
+                        <div class="">
+                            <p class="mb-5">I am working on a blogging application in Laravel 8. There are 4 user
+                                roles, among which, the ...</p>
+                        </div>
+                        <div class="row">
+                            <div class="col me-1 me-lg-2">
+                                <a href="#"><span class="badge rounded-pill text-bg-light">Eloquent</span></a>
+                            </div>
+                            <div class="col-5 col-lg-7">
+                                <div class="avatar-sm-wrapper d-inline-block">
+                                    <a href="#" class="me-1">
+                                        <img class="avatar rounded-circle"
+                                            src="{{ url('assets/images/avatar-sm.png') }}" alt="">
+                                    </a>
+                                </div>
+                                <span class="fs-12px">
+                                    <a href="#" class="me-1 fw-bold">dhimas</a>
+                                    <span class="color-gray">7 hours ago</span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container min-h-372px d-flex flex-column align-items-center justify-content-center">
+        <h2>Ready to contribute?</h2>
+        <p class="mb-4">Want to make a big impact?</p>
+        <div class="text-center">
+            <a href="#" class="btn btn-primary me-2 mb-2 mb-lg-0">Sign Up</a>
+            <a href="#" class="btn btn-secondary mb-2 mb-lg-0">Join Discussion</a>
+        </div>
+    </section>
+    <footer class="py-80px">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-6 mb-5 mb-lg-0">
+                    <img class="h-32px mb-4" src="{{ url('assets/images/logo-blue.png') }}" alt="Logo Laraccus">
+                    <p class="text-white">Empowered by Laravel <br> Laracuss is a community forum for developers of all
+                        skill levels.</p>
+                </div>
+                <div class="col-12 col-lg-6 me-auto">
+                    <div class="d-flex flex-column flex-lg-row justify-content-end">
+                        <div class="d-flex flex-column me-140px mb-3 mb-lg-0">
+                            <p class="fw-bold fs-5 text-white text-nowrap">
+                                Contact Us
+                            </p>
+                            <ul class="list-unstyled">
+                                <li class="text-white"><a href="mailto:hi@laracuss.com">hi@laracuss.com</a></li>
+                            </ul>
+                        </div>
+                        <div class="d-flex flex-column">
+                            <p class="fw-bold fs-5 text-white text-nowrap">
+                                Links
+                            </p>
+                            <ul class="list-unstyled">
+                                <li class="text-white mb-2"><a href="#">Home</a></li>
+                                <li class="text-white mb-2"><a href="#">Discussion</a></li>
+                                <li class="text-white mb-2"><a href="#">About Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 </body>
