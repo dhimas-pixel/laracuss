9e15519d12dbee286e971567951caebb28fa7b32
Dhimas_Mbuh <ephanzen4@gmail.com>
2024-05-20 15:33:01 +0700

    Integration Login

---
+++ database/migrations/2024_05_20_034131_create_discussions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discussions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content_preview');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discussions');
+    }
+};
